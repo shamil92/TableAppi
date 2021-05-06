7c7aa18e326b63c3b0babd1158b32ee1384865b5
shamil92 <kammaev@mail.ru>
2021-05-06 13:52:34 +0300

    Initial Commit

---
+++ TableAppi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableAppi
+//
+//  Created by Шамиль Каммаев on 06.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
